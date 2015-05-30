5d8b16779a3d871fae51e3e6105e03eaed774da8
Norbert Potocki <norbert.potocki@nort.pl>
2015-05-30 00:46:51 -0700

    SimpleConfigurationProvider

---
+++ src/main/java/pl/nort/SimpleConfigurationProvider.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Norbert Potocki (norbert.potocki@nort.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.nort;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+public class SimpleConfigurationProvider implements ConfigurationProvider {
+
+  private final ConfigurationSource configurationSource;
+
+  public SimpleConfigurationProvider(ConfigurationSource configurationSource) {
+    this.configurationSource = checkNotNull(configurationSource);
+  }
+
+  @Override
+  public Properties allConfigurationAsProperties() {
+    try {
+      return configurationSource.getConfiguration();
+    } catch (IllegalStateException e) {
+      throw new IllegalStateException("Couldn't fetch configuration from configuration source", e);
+    }
+  }
+
+  @Override
+  public String getProperty(String key) {
+    try {
+
+      String property = configurationSource.getConfiguration().getProperty(key);
+
+      if (property == null) {
+        throw new NoSuchElementException("No configuration with key: " + key);
+      }
+
+      return property;
+
+    } catch (IllegalStateException e) {
+      throw new IllegalStateException("Couldn't fetch configuration from configuration source for key: " + key, e);
+    }
+  }
+}
---
+++ src/test/java/pl/nort/SimpleConfigurationProviderTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 Norbert Potocki (norbert.potocki@nort.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pl.nort;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleConfigurationProviderTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private SimpleConfigurationProvider simpleConfigurationProvider;
+
+  @Mock
+  private ConfigurationSource configurationSource;
+
+  @Before
+  public void setUp() throws Exception {
+    simpleConfigurationProvider = new SimpleConfigurationProvider(configurationSource);
+  }
+
+  @Test
+  public void shouldThrowWhenUnableToFetchConfiguration() throws Exception {
+    when(configurationSource.getConfiguration()).thenThrow(IllegalStateException.class);
+
+    expectedException.expect(IllegalStateException.class);
+    simpleConfigurationProvider.allConfigurationAsProperties();
+  }
+
+  @Test
+  public void shouldThrowWhenFetchingNonexistentKey() throws Exception {
+    when(configurationSource.getConfiguration()).thenReturn(new Properties());
+
+    expectedException.expect(NoSuchElementException.class);
+    simpleConfigurationProvider.getProperty("some.property");
+  }
+
+  @Test
+  public void shouldThrowWhenUnableToFetchKey() throws Exception {
+    when(configurationSource.getConfiguration()).thenThrow(IllegalStateException.class);
+
+    expectedException.expect(IllegalStateException.class);
+    simpleConfigurationProvider.getProperty("some.property");
+  }
+}

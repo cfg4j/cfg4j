4d939f6329e6cca31ce505acf2dd1632951d5e06
Norbert Potocki <norbert.potocki@nort.pl>
2015-06-03 18:34:25 -0700

    use local git repository in integration tests

--- src/test/java/pl/nort/config/source/GitConfigurationSourceIntegrationTest.java
+++ src/test/java/pl/nort/config/source/GitConfigurationSourceIntegrationTest.java
@@ -17,33 +17,104 @@ package pl.nort.config.source;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.data.MapEntry;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
 
 public class GitConfigurationSourceIntegrationTest {
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
 
+  private SampleGitRepo remoteRepo;
+
+  @Before
+  public void setUp() throws Exception {
+    remoteRepo = new SampleGitRepo();
+    remoteRepo.changeProperty("application.properties", "some.setting", "value");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    remoteRepo.close();
+  }
+
   @Test
   public void shouldThrowWhenUnableToCreateLocalCloneOnNoTempDir() throws Exception {
     expectedException.expect(GitConfigurationSourceException.class);
-    new GitConfigurationSource("https://github.com/nort/config-git-sample-config.git", "/someNonexistentDir/lkfjalfcz", "existing-path");
+    new GitConfigurationSource(remoteRepo.getURI(), "/someNonexistentDir/lkfjalfcz", "existing-path");
   }
 
   @Test
   public void shouldThrowOnInvalidRemote() throws Exception {
     expectedException.expect(GitConfigurationSourceException.class);
-    new GitConfigurationSource("https://github.com/nort/nonExistentRepo");
+    new GitConfigurationSource(File.createTempFile("temp", "temp").getAbsolutePath());
   }
 
   @Test
   public void shouldReadConfigFromRemoteRepository() throws Exception {
-    String repoCoordinates = "https://github.com/nort/config-git-sample-config.git";
-
-    try (GitConfigurationSource gitConfigurationService = new GitConfigurationSource(repoCoordinates)) {
-      assertThat(gitConfigurationService.getConfiguration()).containsKey("some.setting");
+    try (GitConfigurationSource gitConfigurationSource = new GitConfigurationSource(remoteRepo.getURI())) {
+      assertThat(gitConfigurationSource.getConfiguration()).contains(MapEntry.entry("some.setting", "value"));
     }
   }
+
+  private static class SampleGitRepo {
+
+    private final Git repo;
+
+    public SampleGitRepo() throws IOException, GitAPIException {
+      File tempFile = File.createTempFile("test-repo.git", "");
+      repo = createLocalRepo(tempFile);
+    }
+
+    public String getURI() {
+      return repo.getRepository().getWorkTree().getAbsolutePath();
+    }
+
+    public void changeProperty(String propFilePath, String key, String value) throws IOException, GitAPIException {
+      writePropertyToFile(propFilePath, key, value);
+      commitChangesTo(repo);
+    }
+
+    private Git createLocalRepo(File path) throws IOException, GitAPIException {
+      path.delete();
+      return Git.init()
+          .setDirectory(path)
+          .call();
+    }
+
+    private void writePropertyToFile(String propFilePath, String key, String value) throws IOException {
+      OutputStream out = new FileOutputStream(getURI() + "/" + propFilePath);
+      Properties properties = new Properties();
+      properties.put(key, value);
+      properties.store(out, "");
+      out.close();
+    }
+
+    private void commitChangesTo(Git repo) throws GitAPIException {
+      repo.add()
+          .addFilepattern(".")
+          .call();
+
+      repo.commit()
+          .setMessage("config change")
+          .call();
+    }
+
+    public void close() {
+      repo.close();
+    }
+  }
+
 }

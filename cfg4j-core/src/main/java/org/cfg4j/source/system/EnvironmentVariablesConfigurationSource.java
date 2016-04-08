7e2457b150ea72fbef68012cfde3613411003d56
Michal Wysocki <michal@recombine.com>
2016-04-08 01:31:27 -0400

    Adds ConfigurationSources for system properties and environment variables

---
+++ cfg4j-core/src/main/java/org/cfg4j/source/system/EnvironmentVariablesConfigurationSource.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015 Norbert Potocki (norbert.potocki@nort.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cfg4j.source.system;
+
+import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.context.environment.Environment;
+
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * {@link ConfigurationSource} providing all environment variables.
+ */
+public class EnvironmentVariablesConfigurationSource implements ConfigurationSource {
+
+    @Override
+    public Properties getConfiguration(Environment environment) {
+        Properties properties = new Properties();
+
+        Map<String, String> env = System.getenv();
+        properties.putAll(env);
+
+        return properties;
+    }
+
+    @Override
+    public void init() {
+        // NOP
+    }
+
+    @Override
+    public void reload() {
+        // NOP
+    }
+
+    @Override
+    public String toString() {
+        return "EnvironmentVariablesConfigurationSource{}";
+    }
+}
---
+++ cfg4j-core/src/test/java/org/cfg4j/source/system/SystemPropertiesConfigurationSourceTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 Norbert Potocki (norbert.potocki@nort.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cfg4j.source.system;
+
+import org.cfg4j.source.context.environment.DefaultEnvironment;
+import org.cfg4j.source.context.environment.Environment;
+import org.cfg4j.source.empty.EmptyConfigurationSource;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SystemPropertiesConfigurationSourceTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private SystemPropertiesConfigurationSource source;
+
+  @Before
+  public void setUp() throws Exception {
+    source = new SystemPropertiesConfigurationSource();
+    source.init();
+  }
+
+  @Test
+  public void shouldReturnOSName() throws Exception {
+    assertThat(source.getConfiguration(new DefaultEnvironment())).containsKey("os.name");
+  }
+
+  @Test
+  public void reloadShouldNotChangeConfiguration() throws Exception {
+    source.reload();
+    assertThat(source.getConfiguration(new DefaultEnvironment())).containsKey("os.name");
+  }
+
+  @Test
+  public void shouldReturnOsNameForAnyEnvironment() throws Exception {
+    assertThat(source.getConfiguration(mock(Environment.class))).containsKey("os.name");
+  }
+
+  @Test
+  public void shouldNotReturnUndefinedKeys() throws Exception {
+    assertThat(source.getConfiguration(new DefaultEnvironment())).doesNotContainKey("missing.property");
+  }
+
+  @Test
+  public void shouldReturnManuallyDefinedKeys() throws Exception {
+    System.setProperty("manually.added.property", "defined");
+    assertThat(source.getConfiguration(new DefaultEnvironment())).containsKey("manually.added.property");
+  }
+
+  @Test
+  public void shouldReturnManuallyDefinedKeysAfterReload() throws Exception {
+    assertThat(source.getConfiguration(new DefaultEnvironment())).doesNotContainKey("reloaded.property");
+    System.setProperty("reloaded.property", "defined");
+    assertThat(source.getConfiguration(new DefaultEnvironment())).containsKey("reloaded.property");
+  }
+}

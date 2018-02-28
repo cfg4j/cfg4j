309df5b366989e907e0cbb887eac5bc9faeb9b93
pcummuskey <pcummuskey@bravurasolutions.com>
2018-02-28 15:12:11 +1300

    Adding the same simple overloads to ClasspathConfigurationSource.

--- cfg4j-core/src/main/java/org/cfg4j/source/classpath/ClasspathConfigurationSource.java
+++ cfg4j-core/src/main/java/org/cfg4j/source/classpath/ClasspathConfigurationSource.java
@@ -21,6 +21,8 @@ import org.cfg4j.source.ConfigurationSource;
 import org.cfg4j.source.context.environment.Environment;
 import org.cfg4j.source.context.environment.MissingEnvironmentException;
 import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
+import org.cfg4j.source.context.filesprovider.DefaultConfigFilesProvider;
+import org.cfg4j.source.context.filesprovider.SimpleConfigFilesProvider;
 import org.cfg4j.source.context.propertiesprovider.JsonBasedPropertiesProvider;
 import org.cfg4j.source.context.propertiesprovider.PropertiesProvider;
 import org.cfg4j.source.context.propertiesprovider.PropertiesProviderSelector;
@@ -56,14 +58,27 @@ public class ClasspathConfigurationSource implements ConfigurationSource {
    * calls (see corresponding javadoc for detail).
    */
   public ClasspathConfigurationSource() {
-    this(new ConfigFilesProvider() {
-      @Override
-      public Iterable<Path> getConfigFiles() {
-        return Collections.singletonList(
-            Paths.get("application.properties")
-        );
-      }
-    });
+    this(new DefaultConfigFilesProvider());
+  }
+
+  /**
+   * Construct {@link ConfigurationSource} backed by classpath files. Uses the provided path strings to locate the
+   * needed files within the {@link Environment} provided to {@link #getConfiguration(Environment)} calls (see
+   * corresponding javadoc for detail).
+   * @param paths The paths to read from.
+   */
+  public ClasspathConfigurationSource(String... paths) {
+    this(new SimpleConfigFilesProvider(paths));
+  }
+
+  /**
+   * Construct {@link ConfigurationSource} backed by classpath files. Uses the provided paths to locate the needed
+   * files within the {@link Environment} provided to {@link #getConfiguration(Environment)} calls (see corresponding
+   * javadoc for detail).
+   * @param paths The paths to read from.
+   */
+  public ClasspathConfigurationSource(Path... paths) {
+    this(new SimpleConfigFilesProvider(paths));
   }
 
   /**

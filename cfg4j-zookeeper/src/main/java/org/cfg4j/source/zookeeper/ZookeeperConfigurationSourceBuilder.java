0e08a00cf792d6e3942a3563a2ffac0362af7a08
secondriver <secondriver@yeah.net>
2019-07-31 00:22:44 +0800

    Add copyright information

--- cfg4j-zookeeper/src/main/java/org/cfg4j/source/zookeeper/ZookeeperConfigurationSourceBuilder.java
+++ cfg4j-zookeeper/src/main/java/org/cfg4j/source/zookeeper/ZookeeperConfigurationSourceBuilder.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2019 secondriver (secondriver@yeah.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.cfg4j.source.zookeeper;
 
 import java.nio.charset.StandardCharsets;
@@ -8,7 +25,7 @@ import java.nio.charset.StandardCharsets;
  */
 public class ZookeeperConfigurationSourceBuilder {
   
-  private String rootPath = "/cf4j";
+  private String rootPath = "/cfg4j";
   
   private String connectString = "localhost:2181";
   
